55afc5e24b90863bea155fb4e9b99d866be792dd
mdmamunfullstack <mamun9398263@gmail.com>
2021-02-28 23:34:39 +0600

    CustomerListComponent

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/shared/customer.model';
+import { CustomerService} from '../../shared/customer.service';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.scss']
 })
 export class CustomerListComponent implements OnInit {
-
-  constructor() { }
-
+  allCustomers: Observable<Customer[]>;  
+  constructor(public custservice: CustomerService,
+                       private toastr : ToastrService) { }
   ngOnInit() {
+    this.loadAllCustomers();  
   }
-
+  loadAllCustomers() {  
+    this.allCustomers = this.custservice.getCustomerList();  
+  }  
+  getCustomerList() {  
+   this.custservice.getCustomerList();  
+  }  
+  showForEdit(cust: Customer) {
+    debugger;;
+    this.custservice.selectedCustomer = Object.assign({}, cust);;
+  }
+  onDelete(id: number) {
+    debugger;
+    if (confirm('Are you sure to delete this record ?') == true) {
+      this.custservice.deleteCustomer(id)
+      .subscribe(x => {
+        this.custservice.getCustomerList();
+        this.toastr.warning("Deleted Successfully","Employee Register");
+      })
+    }
+  }
 }
