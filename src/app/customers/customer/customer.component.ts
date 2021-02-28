f8bb9eb36339b2d5cf186c7ee54aecc1f71d058b
mdmamunfullstack <mamun9398263@gmail.com>
2021-02-28 23:31:26 +0600

    add reset and submit method

--- src/app/customers/customer/customer.component.ts
+++ src/app/customers/customer/customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { CustomerService } from '../../shared/customer.service'
 
 @Component({
   selector: 'app-customer',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  custIdUpdate = null; 
+  constructor(public custservice: CustomerService, private toastr: ToastrService) { 
+    
   }
+
+  ngOnInit():void {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    debugger;
+    if (form != null)
+      form.reset();
+    this.custservice.selectedCustomer = {
+      CustId: 0,
+      Name: '',
+      Address: '',
+      Mobile: '',
+      Email: ''
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    debugger;
+    console.log(form.value.CustId);
+    if(form.value.CustId==null)
+    {
+      form.value.CustId=0;
+    }
+    this.custservice.selectedCustomer.CustId=form.value.CustId
+    if (this.custservice.selectedCustomer.CustId == 0) {
+      this.custservice.addCustomer(form.value)
+        .subscribe(data => {
+          form.value.CustId=0;
+          this.resetForm(form);
+          this.custservice.getCustomerList();
+          this.toastr.success('New Record Added Succcessfully', 'Customer Registration');
+        })
+    }
+    else {
+      console.log("hello");
+      this.custservice.updateCustomer(form.value.CustId, form.value)
+      .subscribe(data => {
+        this.resetForm(form);
+        this.custservice.getCustomerList();
+        this.toastr.info('Record Updated Successfully!', 'Customer Registration');
+      });
+    }
+  }
+    
 
 }
